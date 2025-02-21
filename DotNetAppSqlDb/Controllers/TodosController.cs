c161f7536ad9eb749798e2d89ceccaf07d0e9811
apc-sh-tanaka <sh_tanaka@ap-com.co.jp>
2025-02-21 09:34:25 +0000

    comment out @Html.AntiForgeryToken()

--- DotNetAppSqlDb/Controllers/TodosController.cs
+++ DotNetAppSqlDb/Controllers/TodosController.cs
@@ -10,7 +10,6 @@ using DotNetAppSqlDb.Models;using System.Diagnostics;
 
 namespace DotNetAppSqlDb.Controllers
 {
-    [Authorize]
     public class TodosController : Controller
     {
         private MyDatabaseContext db = new MyDatabaseContext();
